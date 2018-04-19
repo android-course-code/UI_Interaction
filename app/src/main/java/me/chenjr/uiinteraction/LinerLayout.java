b6544febc596264691a8fb778e62bb085719bbcc
chenjr15 <icjr@qq.com>
2018-04-19 11:58:42 +0800

    添加一个新布局

---
+++ app/src/main/java/me/chenjr/uiinteraction/LinerLayout.java
@@ -0,0 +1,22 @@
+package me.chenjr.uiinteraction;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LinerLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liner_layout);
+//        TODO:
+//        LinerLayout linerLayout = (LinerLayout) this.findViewById(R.id.Linear_layout_uc1);
+//        linerLayout.setOrientation();
+
+    }
+
+}
--- app/src/main/java/me/chenjr/uiinteraction/MainActivity.java
+++ app/src/main/java/me/chenjr/uiinteraction/MainActivity.java
@@ -1,7 +1,10 @@
 package me.chenjr.uiinteraction;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button uc1 =(Button) findViewById(R.id.btn_main_uc1);
+        uc1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent());
+            }
+        });
+
+
     }
 }
